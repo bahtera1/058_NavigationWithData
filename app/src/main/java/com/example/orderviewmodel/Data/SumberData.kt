33350a57c365e99d1641060d41de4c33f6e8d210
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-11-09 03:42:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/orderviewmodel/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.orderviewmodel.Data
+
+import com.example.orderviewmodel.R
+
+object SumberData {
+    val flavours = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
---
+++ app/src/main/java/com/example/orderviewmodel/OrderViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.orderviewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.orderviewmodel.Data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.text.NumberFormat
+
+
+private const val HARGA_PER_CUP = 3000
+
+class OrderViewModel : ViewModel() {
+    private val _stateUI = MutableStateFlow(OrderUIState())
+    val  stateUI:StateFlow<OrderUIState> = _stateUI.asStateFlow()
+
+    fun setJumlah(jmlhEsJumbo:Int){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                jumlah = jmlhEsJumbo,
+                harga = hitunganHarga(jumlah=jmlhEsJumbo)
+            )
+        }
+    }
+
+    fun setRasa(rasaPilihan:String){
+        _stateUI.update { stateSaatini ->
+            stateSaatini.copy(rasa=rasaPilihan)
+        }
+    }
+    fun  resetOrder(){
+        _stateUI.value= OrderUIState()
+    }
+    private fun hitunganHarga(
+        jumlah:Int = _stateUI.value.jumlah,
+    ): String{
+        val KalkulasiHarga =jumlah * HARGA_PER_CUP
+
+        return NumberFormat.getNumberInstance().format(KalkulasiHarga)
+    }
+}
