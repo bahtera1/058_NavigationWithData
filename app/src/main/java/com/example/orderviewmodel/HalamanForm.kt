d1477d2cfa93184ebee934458f2290b1eea54e1a
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-11-16 16:53:23 +0700

    tambah Texfield Nama, Alamat dan telepon di HalamanForm.kt

--- app/src/main/java/com/example/orderviewmodel/HalamanForm.kt
+++ app/src/main/java/com/example/orderviewmodel/HalamanForm.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
     onSubmitButtonClick:(MutableList<String>) -> Unit
@@ -46,6 +49,21 @@ fun HalamanForm(
             fontSize = 24.sp,
             modifier = Modifier.padding(bottom = 16.dp)
         )
+        OutlinedTextField(value = namaTxt, onValueChange = {
+            namaTxt = it
+        }, label = {
+            Text(text = "Masukkan Nama")
+        })
+        OutlinedTextField(value = alamatTxt, onValueChange = {
+            alamatTxt = it
+        }, label = {
+            Text(text = "Masukkan Alamat")
+        })
+        OutlinedTextField(value = telponTxt, onValueChange = {
+            telponTxt= it
+        }, label = {
+            Text(text = "Masukkan No Telepon")
+        })
 
     }
 }
