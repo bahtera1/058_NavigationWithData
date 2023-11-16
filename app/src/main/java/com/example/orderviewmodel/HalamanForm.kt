340781a8d9160d19ec6d661a3880923e9c3ecc6e
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-11-16 16:40:55 +0700

    tambah column and text di HalamanForm.kt

--- app/src/main/java/com/example/orderviewmodel/HalamanForm.kt
+++ app/src/main/java/com/example/orderviewmodel/HalamanForm.kt
@@ -1,11 +1,21 @@
 package com.example.orderviewmodel
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -23,4 +33,19 @@ fun HalamanForm(
     }
     var listDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
 
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){
+        Text(
+            text = "Data Pelanggan",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+    }
 }
