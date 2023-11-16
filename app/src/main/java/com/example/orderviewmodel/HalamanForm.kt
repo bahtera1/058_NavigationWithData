3f341002eb5ec61a4a95ac4299768c2533bdb1fa
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-11-16 16:24:58 +0700

    tambah function HalamanForm di HalamanForm.kt

--- app/src/main/java/com/example/orderviewmodel/HalamanForm.kt
+++ app/src/main/java/com/example/orderviewmodel/HalamanForm.kt
@@ -1,2 +1,26 @@
 package com.example.orderviewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick:(MutableList<String>) -> Unit
+) {
+    var namaTxt by rememberSaveable {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var telponTxt by remember {
+        mutableStateOf("")
+    }
+    var listDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+
+}
